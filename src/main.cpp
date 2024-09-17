b26787536fe370e0c5a083fad92e4ca5a36ef255
tr4shp4ndu <46363873+Tr4shP4ndu@users.noreply.github.com>
2024-09-17 23:08:18 +0100

    Tidy up game code, add random to block appearing and next one

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <raylib.h>
-#include "grid.h"
-#include "blocks.cpp"
+#include "game.h"
 
 int main()
 {
@@ -8,18 +7,13 @@ int main()
     SetTargetFPS(60); // Set the target FPS to 60 for smooth gameplay
     Color darkBlue = {44, 44, 127, 255}; // Custom dark blue background color
 
-    Grid grid; // Create a Grid object that manages the game grid
-    grid.Print(); // Output initial grid state (for debugging)
-    ZBlock block; // Create a ZBlock object (for testing block drawing)
-    block.Move(4,3);
-
+    Game game = Game();
     
     while (!WindowShouldClose()) // Main game loop - runs while the window is not closed
     {
         BeginDrawing(); // Begin rendering
         ClearBackground(darkBlue); // Clear the background with a dark blue color
-        grid.Draw(); // Draw the grid and the current block
-        block.Draw();
+        game.Draw();
         EndDrawing(); // Finish rendering
     }
 
