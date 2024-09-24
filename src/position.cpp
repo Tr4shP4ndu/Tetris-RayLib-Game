6ebb1bc67b385fccd6760a2f8a8dd673ee9e8370
tr4shp4ndu <46363873+Tr4shP4ndu@users.noreply.github.com>
2024-09-24 23:51:44 +0100

    o

--- src/position.cpp
+++
@@ -1,8 +0,0 @@
-#include "position.h"
-
-// Constructor definition for Position class
-Position::Position(int row, int column) // Initializes the row and column members with the given values
-{
-    this->row = row; // 'this->' is not necessary here unless you want to emphasize that
-    this->column = column; // you're referring to the member variables of the class.
-}
